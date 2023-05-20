9747a8a96042e4991a57f19360ee9e59ce507cb2
Maxi Pisso <maximiliano.pisso@gmail.com>
2023-05-20 14:05:08 -0300

    estructuracion archivos

---
+++ Archivos Base/Wallet/classes/movimiento.js
@@ -0,0 +1,15 @@
+class Movimiento {
+
+    constructor(id,date,lote,proceso,puntaje,dias,vencimiento){
+
+        this.id = id;
+        this.fecha = date; 
+        this.lote = lote;
+        this.proceso = proceso;
+        this.puntaje = puntaje;
+        this.diasEsteriles = dias;
+        this.vencimiento = vencimiento;
+        
+
+    }
+}
---
+++ README copy.md
@@ -0,0 +1,2 @@
+# ISI_Entornos_Web
+Trabajo final para la materia de Entornos Web - UCEL - 2023
